044a1e5b9b4e014c3386b7cbcc561d515453ef36
Aymeric <ssabatieraymeric@gmail.com>
2024-02-02 00:37:42 +0100

    [imp]Fonctionne presque

--- miniprojet.js
+++ miniprojet.js
@@ -2,7 +2,8 @@
 'use strict';
 
 import * as math from 'mathjs';
-import {ndc,perspective, translation, yRotation, zRotation, xRotation} from '/matrices';
+import {ndc,perspective, translation, yRotation, zRotation, xRotation, scaling} from '/matrices';
+import { AnimationMixer } from 'three';
 
 //====================================
 // Récupération canvas + WebGL
@@ -19,28 +20,22 @@ if (!gl) {
 //====================================
 const vertex_GLSL = `#version 300 es
 in vec3 a_position;
-in vec3 a_color;
 uniform mat4 u_projectionMatrix;
 uniform mat4 u_modelViewMatrix; 
 
-out vec4 v_color;
-
 void main() {
   gl_Position = u_projectionMatrix * u_modelViewMatrix * vec4(a_position,1);
-
-  v_color = vec4(a_color,1);
 }
 `;
 
 const fragment_GLSL = `#version 300 es
 precision highp float;
 
-in vec4 v_color;
-
 out vec4 outColor;
+uniform vec4 u_color;
 
 void main() {
-   outColor = v_color;
+  outColor = u_color;
 }
 `;
 
@@ -51,7 +46,7 @@ const prg = creation_programme_shading(gl, [
 
 // Localisation des attributs
 const positionLocation = gl.getAttribLocation(prg, "a_position");
-const colorLocation = gl.getAttribLocation(prg, "a_color");
+const colorLocation = gl.getUniformLocation(prg, "u_color");
 // Localisation des uniforms
 const projectionMatrixLocation = gl.getUniformLocation(prg, "u_projectionMatrix");
 const modelViewMatrixLocation = gl.getUniformLocation(prg, "u_modelViewMatrix");
@@ -70,16 +65,9 @@ gl.bindVertexArray(vao);
 // Spécifier le programme utilisé (i.e. les shaders utilisés)
 gl.useProgram(prg);
 
-//color
-const colorBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-setColors(gl);
-gl.enableVertexAttribArray(colorLocation);
-gl.vertexAttribPointer(colorLocation, 3, gl.UNSIGNED_BYTE, true, 0, 0);
-
 const fieldOfViewInRadians = degToRad(30);
 const zNear = 1; //1
-const zFar = 2000;//2000;
+const zFar = 3000;//2000;
 const h = zNear*Math.tan(0.6*fieldOfViewInRadians);
 const w = h*canvas.clientWidth/canvas.clientHeight;
 //const aspect = canvas.clientWidth / canvas.clientHeight;
@@ -92,7 +80,7 @@ gl.uniformMatrix4fv(projectionMatrixLocation, false, math.flatten(math.transpose
 const cameraUP = degToRad(0);
 let cameraMatrix = zRotation(cameraUP);
 //    visée : position+orientation
-const radius = 300; // la position de la caméra dépend du rayon du F-henge
+const radius = 700; // la position de la caméra dépend du rayon du F-henge
 const cameraAngleRadians = degToRad(-40);
 let cameraMatrixInit = translation(0, 0, radius * 3.0);
 cameraMatrixInit = math.multiply( xRotation(cameraAngleRadians) , cameraMatrixInit );
@@ -109,7 +97,7 @@ let cameraPosRadians;
 function draw(inversedCameraMatrixWorld, points, position, color){
   const primitiveType = gl.TRIANGLES;
   const offset = 0;
-  const count = 96; // 16 tuiles de 2 triangles avec 3 vertices chacun
+  const count = points.length/3; // 16 tuiles de 2 triangles avec 3 vertices chacun
 
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -122,12 +110,9 @@ function draw(inversedCameraMatrixWorld, points, position, color){
   // In GLSL matrices are transposed!!!
   gl.uniformMatrix4fv(modelViewMatrixLocation, false, math.flatten(math.transpose(modelViewMatrix)).valueOf());
 
+  gl.uniform4fv(colorLocation, color);
 
-
-  // Dessiner
-
-
-  gl.drawArrays(primitiveType, offset, count);
+  gl.drawArrays(primitiveType, offset, count  );
 }
 
 drawScene();
@@ -136,7 +121,7 @@ function drawScene() {
   gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clearColor(0.5, 0.7, 1.0, 1.0); // couleur du canvas et non du viewport
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  gl.enable(gl.CULL_FACE);
+  //gl.enable(gl.CULL_FACE);
   gl.enable(gl.DEPTH_TEST);
 
   // Mise à jour des uniforms (et attributes)
@@ -146,22 +131,51 @@ function drawScene() {
 
   const inversedCameraMatrixWorld = math.inv(cameraMatrix);
 
+
+
+  let matriceCentre = [0,0,0]
+  matriceCentre = translation(matriceCentre[0], matriceCentre[1], matriceCentre[2]);
+
   let matriceTour = [0,0,0];
   matriceTour = translation(matriceTour[0], matriceTour[1], matriceTour[2]);
 
   let matriceSol = [0,0,0];
   matriceSol = translation(matriceSol[0], matriceSol[1], matriceSol[2]);
-  matriceSol = math.multiply(matriceSol, yRotation(degres));
 
-  let matriceCube = [100,150,0];
+  let matriceTriangle = [250,100,0];
+  matriceTriangle = translation(matriceTriangle[0], matriceTriangle[1], matriceTriangle[2]);
+
+  let matriceCones = [-300, 100, 0];
+  matriceCones = translation(matriceCones[0], matriceCones[1], matriceCones[2]);
+
+  let matriceArbre = [-150, 100, -450];
+  matriceArbre = translation(matriceArbre[0], matriceArbre[1], matriceArbre[2]);
+
+  let matriceCube = [100,10,0];
   degres = degres + 0.01;
   matriceCube = translation(matriceCube[0], matriceCube[1], matriceCube[2]);
   matriceCube = math.multiply(matriceCube, zRotation(degres));
-  matriceCube = math.multiply(matriceCube, xRotation(degres));
+  matriceCube = math.multiply(matriceCube, yRotation(degres));
 
-  draw(inversedCameraMatrixWorld, getTourGeometry(), matriceTour, [0, 0, 0, 1]);
-  draw(inversedCameraMatrixWorld, getSol(), matriceSol, [0, 0, 0, 1]);
-  draw(inversedCameraMatrixWorld, getCubeGeometry(), matriceCube, [0, 0, 0, 1]);
+  draw(inversedCameraMatrixWorld, getTourGeometry(), matriceTour, [250, 0, 0, 1]);
+  draw(inversedCameraMatrixWorld, getSol(), matriceSol, [0, 250, 0, 1]);
+  draw(inversedCameraMatrixWorld, getformetriangulaire(), matriceTriangle, [0, 150, 150, 1]);
+  draw(inversedCameraMatrixWorld, getFormCones(), matriceCones, [150, 150, 0, 1]);
+  draw(inversedCameraMatrixWorld, getFormArbre(), matriceArbre, [150, 0, 150, 1]);
+  let numObjet = 5
+  for (let i =0; i < numObjet; i++){
+    let radiusCube = 100;
+    let angle = i * Math.PI * 2 / numObjet;
+    let x = Math.cos(angle) * radiusCube;
+    let y = Math.sin(angle) * radiusCube;
+    matriceCube = [x,200, -y];
+    matriceCube = translation(matriceCube[0], matriceCube[1], matriceCube[2]);
+    matriceCube = math.multiply(matriceCube, zRotation(degres));
+    matriceCube = math.multiply(matriceCube, xRotation(degres));
+    matriceCube = math.multiply(matriceCube, translation(x, 0, -y));
+    
+    draw(inversedCameraMatrixWorld, getCubeGeometry(), matriceCube, [0, 0, 250, 1]);
+  }
 
   requestAnimationFrame(drawScene);
 }
